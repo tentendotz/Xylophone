9844e4c88537d4557f4568488370771706c332d3
ttt-net <tenten102matsuyama@gmail.com>
2022-05-27 01:01:26 +0900

    Add pressed animation

--- Xylophone/ViewController.swift
+++ Xylophone/ViewController.swift
@@ -13,11 +13,14 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
     
     @IBAction func keyPressed(_ sender: UIButton) {
         playSound(soundName: sender.currentTitle!)
+        sender.alpha = 0.5
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.2) {
+            sender.alpha = 1.0
+        }
     }
     
     func playSound(soundName: String) {
